730f41d8db8c215e6d3ca8b0a4f2a9b8a2422c71
Gerolmed <werwolf420@yahoo.de>
2022-03-11 12:42:56 +0100

    Added importer and example syntax file

---
+++ Core/Content/States/test.lua
@@ -0,0 +1,35 @@
+stateBuilder("my_state")
+        :render(
+                function(renderer, context) 
+                    local text = renderer.addText("example.message.1")
+                    renderer.addText("example.message.2")
+                    renderer.addAction(StateChangeAction("example.button.1", "my_other_state"))
+                    renderer.addAction(CustomAction("example.button.1", 
+                            function()
+                                text.setMessage("example.message.3")
+                                context.ClearActions()
+                                context.runLater(1000*2, 
+                                        function()  
+                                            context.changeState("my_other_state")
+                                        end
+                                )
+                            end
+                    ))
+                end
+        )
+        --.showExit(false) defaults to false anyways
+        :build()
+
+stateBuilder("my_other_state")
+        :render(
+                function(renderer, context)
+                    renderer.addText("example.message.4")
+                    renderer.addAction(StateChangeAction("example.button.1", "my_state"))
+                    renderer.addAction(CustomAction("example.button.3",
+                            function()
+                                context.exit();
+                            end
+                    ))
+                end
+        )
+        :build()
---
+++ PipelineExtension/Lib/LuaScriptProcessor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using TInput = System.String;
+using TOutput = System.String;
+
+namespace PipelineExtension
+{
+    [ContentProcessor(DisplayName = "LuaScriptProcessor")]
+    public class LuaScriptProcessor : ContentProcessor<string, string>
+    {
+        public override string Process(string input, ContentProcessorContext context)
+        {
+            return input;
+        }
+    }
+}
