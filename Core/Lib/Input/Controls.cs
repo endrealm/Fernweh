802a2fb8f0806c2e240d4a764566d2afd60aa673
Pherblo <poraykob@gmail.com>
2022-03-11 23:57:31 -0600

    Add gamepad support

--- Core/Lib/Input/Controls.cs
+++ Core/Lib/Input/Controls.cs
@@ -10,35 +10,42 @@ namespace Core.Input
     public class Controls: IUpdate<TopLeveUpdateContext>
     {
         private static KeyboardSnapshot keyboardSnapshot = new KeyboardSnapshot();
+        private static GamePadSnapshot gamePadSnapshot = new GamePadSnapshot();
 
         public void Update(float deltaTime, TopLeveUpdateContext context)
         {
             keyboardSnapshot.Update(deltaTime, context);
+            gamePadSnapshot.Update(deltaTime, context);
         }
 
         public static bool AnyInput()
         {
-            return keyboardSnapshot.currentKeyState.GetPressedKeys().Length > 0;
+            return keyboardSnapshot.currentKeyState.GetPressedKeys().Length > 0
+                || gamePadSnapshot.AnyButtonPress();
         }
 
         public static bool MoveUp()
         {
-            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.W, Keys.Up });
+            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.W, Keys.Up })
+                || gamePadSnapshot.HasBeenPressed(new Buttons[] { Buttons.DPadUp, Buttons.LeftThumbstickUp });
         }
 
         public static bool MoveDown()
         {
-            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.S, Keys.Down });
+            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.S, Keys.Down })
+                || gamePadSnapshot.HasBeenPressed(new Buttons[] { Buttons.DPadDown, Buttons.LeftThumbstickDown });
         }
 
         public static bool MoveLeft()
         {
-            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.A, Keys.Left });
+            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.A, Keys.Left })
+                || gamePadSnapshot.HasBeenPressed(new Buttons[] { Buttons.DPadLeft, Buttons.LeftThumbstickLeft });
         }
 
         public static bool MoveRight()
         {
-            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.D, Keys.Right });
+            return keyboardSnapshot.HasBeenPressed(new Keys[] { Keys.D, Keys.Right })
+                || gamePadSnapshot.HasBeenPressed(new Buttons[] { Buttons.DPadRight, Buttons.LeftThumbstickRight });
         }
     }
 }
---
+++ Core/Lib/Input/GamePadSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Core.Utils;
+
+namespace Core.Input
+{
+    internal class GamePadSnapshot : IUpdate<TopLeveUpdateContext>
+    {
+        public GamePadState currentButtonState = GamePad.GetState(0);
+        private GamePadState _previousButtonState = GamePad.GetState(0);
+
+        public void Update(float deltaTime, TopLeveUpdateContext context)
+        {
+            _previousButtonState = currentButtonState;
+            currentButtonState = GamePad.GetState(0);
+        }
+
+        public bool HasBeenPressed(Buttons[] buttons)
+        {
+            if (!currentButtonState.IsConnected) return false;
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (currentButtonState.IsButtonDown(buttons[i]) && !_previousButtonState.IsButtonDown(buttons[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public bool AnyButtonPress()
+        {
+            return currentButtonState != _previousButtonState;
+        }
+    }
+}
