329f5a7824108d8f3fcd5fa02e54987b831d3e8f
Gerolmed <werwolf420@yahoo.de>
2022-04-22 12:47:45 +0200

    Added runtime check

--- PipelineChecks/Program.cs
+++ PipelineChecks/Program.cs
@@ -1,28 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using Core.Scenes.Ingame.Battle;
 using Core.Scripting;
 using Core.States;
 using PipelineExtensionLibrary;
 
-new XmlDialogParser().Parse("Some example <color=\"rgba(1,1,1,.4)\">text</color> here!");
+var watch = new Stopwatch();
+var iterations = 1000;
+var parser = new XmlDialogParser();
+watch.Start();
+for (int i = 0; i < iterations; i++)
+{
+    parser.Parse("Some example <color=\"rgba(1,1,1,.4)\">text</color> here! <color=\"rgba(1,1,1,.4)\">text <color=\"rgba(1,1,1,.4)\">text</color> asd</color> asd <color=\"rgba(1,1,1,.4)\">text</color>");
+}
+watch.Stop();
 
-var manager = new ScriptLoader(new StateRegistry(), new BattleRegistry());
+Console.WriteLine("Took: " + (watch.ElapsedMilliseconds / 1000f));
+Console.WriteLine("Per: " + (watch.ElapsedMilliseconds / 1000f)/iterations);
 
-manager.LoadScript(@"
-stateBuilder('my_other_state')
-    :render(
-        function(renderer, context)
-            renderer.addText('example.message.4')
-            renderer.addAction(StateChangeAction('example.button.1', 'my_state'))
-            renderer.addAction(CustomAction('example.button.3',
-                function()
-                    context.exit();
-                end
-            ))
-        end
-    )
-    :build()
-", new ScriptContext(new NamespacedKey("", ""), ""));
+// var manager = new ScriptLoader(new StateRegistry(), new BattleRegistry());
+// 
+// manager.LoadScript(@"
+// stateBuilder('my_other_state')
+//     :render(
+//         function(renderer, context)
+//             renderer.addText('example.message.4')
+//             renderer.addAction(StateChangeAction('example.button.1', 'my_state'))
+//             renderer.addAction(CustomAction('example.button.3',
+//                 function()
+//                     context.exit();
+//                 end
+//             ))
+//         end
+//     )
+//     :build()
+// ", new ScriptContext(new NamespacedKey("", ""), ""));
 
 Console.WriteLine("Done");
