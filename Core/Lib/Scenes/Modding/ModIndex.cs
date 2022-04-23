61a4005cdbdc176adb40ec6574b3e7b3d039e197
Gerolmed <werwolf420@yahoo.de>
2022-04-23 16:38:11 +0200

    Added mod type value

--- Core/Lib/Scenes/Modding/ModIndex.cs
+++ Core/Lib/Scenes/Modding/ModIndex.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Core.Scenes.Modding;
 
@@ -6,7 +8,16 @@ public class ModIndex
 {
 
     public string Id;
+    
+    [JsonConverter(typeof(StringEnumConverter))]
+    public ModType type;
     public string[] Dependencies = Array.Empty<string>();
     public string[] Scripts = Array.Empty<string>();
 
+}
+
+public enum ModType
+{
+    Library,
+    Game
 }
