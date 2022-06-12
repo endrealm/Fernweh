30f3c9bb5218b75642a1c9ae1828e9a5af623d3d
Pherblo <poraykob@gmail.com>
2022-06-11 22:44:07 -0500

    Remove test code + rename sfx

--- Core/Mods/CoreMod/States/variableMaker.lua
+++ Core/Mods/CoreMod/States/variableMaker.lua
@@ -1,6 +1,6 @@
-﻿local test = Context:CreateVar("test")
-test:Set("example")
-
-Context:CreateFunc("test2", function()
-    print("Example function invoked")
-end)
+﻿--local test = Context:CreateVar("test")
+--test:Set("example")
+--
+--Context:CreateFunc("test2", function()
+--    print("Example function invoked")
+--end)
--- Core/Mods/CoreMod/data.lua
+++ Core/Mods/CoreMod/data.lua
@@ -6,16 +6,3 @@ RegisterSlot("head")
 RegisterSlot("body")
 RegisterSlot("feet")
 RegisterSlot("accessory")
-
-
--- test code below pls remove
-SetDataLoader(function(data)
-    print("Loaded data:")
-    print(data)
-end)
-
-SetDataSaver(function()
-    -- run your logic here
-    return "test value inserted"
-end)
--- test code above pls remove
